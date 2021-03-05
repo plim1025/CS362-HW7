a4ddbb375837b2057c4a1782f084c510570f0197
plim1025 <plim1025@gmail.com>
2021-03-04 23:08:35 -0700

    Wrote second test, tests leapYear with an input of 400 and expects True

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,6 +4,8 @@ from leapYear import leapYear
 class TestLeapYear(unittest.TestCase):
     def test_leapYear3(self):
         self.assertEqual(leapYear(3), False)
+    def test_leapYear400(self):
+        self.assertEqual(leapYear(400), True)
 
 if __name__ == "__main__":
     unittest.main()
