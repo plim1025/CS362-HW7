f76ec4551bf4ad6b5fd443bb5418fa30c2a94663
plim1025 <plim1025@gmail.com>
2021-03-04 22:49:03 -0700

    Wrote second test, tests FizzBuzz with an input of 5 and expects Buzz

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -1,7 +1,9 @@
 import unittest
 from fizzBuzz import fizzBuzz
 
-class TestFizzBuzz(unittest.Testcase):
+class TestFizzBuzz(unittest.TestCase):
     def test_fizzBuzz3(self):
         self.assertEqual(fizzBuzz(3), 'Fizz')
+    def test_fizzBuzz5(self):
+        self.assertEqual(fizzBuzz(5), 'Buzz')
     
